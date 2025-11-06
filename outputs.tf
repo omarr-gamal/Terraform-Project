91d824b7a4a871c0770b13dac3fba09b7215c1b8
Omar Gamal <omogamal2002@gmail.com>
2025-11-06 20:13:55 +0200

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "public_alb_dns" {
+  value = module.alb_public.lb_dns
+}
+output "internal_alb_dns" {
+  value = module.alb_internal.lb_dns
+}
+
+# map individual IPs for local-exec consumption
+output "public_proxy_ip_1" {
+  value = element(module.public_ec2.public_ips, 0)
+}
+output "public_proxy_ip_2" {
+  value = element(module.public_ec2.public_ips, 1)
+}
+output "private_backend_ip_1" {
+  value = element(module.private_ec2.private_ips, 0)
+}
+output "private_backend_ip_2" {
+  value = element(module.private_ec2.private_ips, 1)
+}
+
